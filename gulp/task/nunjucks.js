1c449ccec0b4020494a30f249a9c4116c68d62bc
dengyingyi2 <dengyingyi@jd.com>
2018-11-15 17:47:01 +0800

    nunjuncks compile

--- gulp/task/nunjucks.js
+++ gulp/task/nunjucks.js
@@ -2,11 +2,12 @@ const gulp = require('gulp');
 const nunjuncks = require('gulp-nunjucks');
 
 /**
- * 
+ * 模板引擎编译
  * 
  */
 
 gulp.task('nunjucks-base', () => {
-    gulp.src('./src/components/base/base.html')
-        .pipe()
+    gulp.src('./src/views/index.html')
+        .pipe(nunjuncks.compile())
+        .pipe(gulp.dest('./.www/views'))
 })
