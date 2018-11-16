2574dc90e2473cb9d09452f6bf3890f4e4e5f641
dengyingyi2 <dengyingyi@jd.com>
2018-11-16 10:20:01 +0800

    dev

--- gulp/task/nunjucks.js
+++ gulp/task/nunjucks.js
@@ -1,15 +1,17 @@
 const gulp = require('gulp');
 const nunjuncks = require('gulp-nunjucks');
+const notify = require('gulp-notify')
 
 /**
  * 模板引擎编译
  * 
  */
 
-gulp.task('nunjucks-base', () => {
+gulp.task('nunjucks', () => {
     gulp.src('./src/views/index.html')
         .pipe(nunjuncks.compile({
             username: 'xxmxm'
         }))
+        .on('error', notify.onError("Error: <%= error.message %>"))
         .pipe(gulp.dest('./.www/views'))
 })
